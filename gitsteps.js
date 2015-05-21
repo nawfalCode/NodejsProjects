a510873b051b4aeb551197ff95c118459cd795ac
Nawfal <nawfalrez@gmail.com>
2015-05-22 04:18:55 +1000

    Git instractions

---
+++ gitsteps.js
@@ -0,0 +1,7 @@
+to add tor repository : git add file1 file2 file3
+to add a folder       : git add .\foldername
+to commit             : git commit -m 'message'
+to push to server     : git push -u origin
+to view branchs       : git remote -v
+set default to simple : git config --global push.default simple 
+to change git URL     : git rempte set-url origin https://github.com/NawfalCode/asdas.git
